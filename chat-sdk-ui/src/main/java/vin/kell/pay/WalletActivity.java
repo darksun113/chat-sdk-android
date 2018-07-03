4594bca55ce97897b55c05f111a70480c9e9e684
darksun113 <mtion@msn.com>
2018-07-03 16:05:58 -0500

    coding...

--- chat-sdk-ui/src/main/java/vin/kell/pay/WalletActivity.java
+++ chat-sdk-ui/src/main/java/vin/kell/pay/WalletActivity.java
@@ -1,15 +1,25 @@
 package vin.kell.pay;
 
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+
+import com.blikoon.qrcodescanner.QrCodeActivity;
 
 import co.chatsdk.ui.R;
 
 public class WalletActivity extends AppCompatActivity {
+    private static final int REQUEST_CODE_QR_SCAN = 101;
+    protected Button btnScanToPay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +36,62 @@ public class WalletActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        btnScanToPay = findViewById(R.id.btnScanPay);
+        btnScanToPay.setOnClickListener(view -> {
+            startScanToPayActivity(WalletActivity.this);
+        });
+    }
+
+    private void startScanToPayActivity(Activity activity) {
+        Intent i = new Intent(activity, QrCodeActivity.class);
+        startActivityForResult(i,REQUEST_CODE_QR_SCAN);
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        if(resultCode != Activity.RESULT_OK)
+        {
+            Log.d("Kelvin","COULD NOT GET A GOOD RESULT.");
+            if(data==null)
+                return;
+            //Getting the passed result
+            String result = data.getStringExtra("com.blikoon.qrcodescanner.error_decoding_image");
+            if( result!=null)
+            {
+                AlertDialog alertDialog = new AlertDialog.Builder(WalletActivity.this).create();
+                alertDialog.setTitle("Scan Error");
+                alertDialog.setMessage("QR Code could not be scanned");
+                alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.dismiss();
+                            }
+                        });
+                alertDialog.show();
+            }
+            return;
+
+        }
+        if(requestCode == REQUEST_CODE_QR_SCAN)
+        {
+            if(data==null)
+                return;
+            //Getting the passed result
+            String result = data.getStringExtra("com.blikoon.qrcodescanner.got_qr_scan_relult");
+            Log.d("Kelvin","Have scan result in your app activity :"+ result);
+            AlertDialog alertDialog = new AlertDialog.Builder(WalletActivity.this).create();
+            alertDialog.setTitle("Scan result");
+            alertDialog.setMessage(result);
+            alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
+                    new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                        }
+                    });
+            alertDialog.show();
+
+        }
     }
 
 }
